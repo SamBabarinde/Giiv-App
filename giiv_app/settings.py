a49ec228474d61dd57648ce20c12724a018948a9
SamBabarinde <69460415+SamBabarinde@users.noreply.github.com>
2024-01-12 12:31:52 +0100

    Update settings.py

--- giiv_app/settings.py
+++ giiv_app/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = 'giiv_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
+        'DIRS': [BASE_DIR / 'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
