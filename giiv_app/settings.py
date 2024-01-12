cbe791768abc347c1068524092d55c7031975748
SamBabarinde <69460415+SamBabarinde@users.noreply.github.com>
2024-01-12 13:14:42 +0100

    Update settings.py

--- giiv_app/settings.py
+++ giiv_app/settings.py
@@ -8,8 +8,6 @@ from django.contrib.messages import constants as messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -66,13 +64,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'giiv_app.urls'
 
-# TEMPLATE_DIRS = (
-#   os.path.join(SETTINGS_PATH, 'Templates'),)
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'Templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
